d307e95d6e7e4d68e26c35f3fa22570efb29e1f0
sp-vamshi <spvamshi22@gmail.com>
2024-01-28 13:28:37 +0530

    configured deploy settings.

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ import { enUS, frFR, zhCN, viVN, arSD } from '@mui/material/locale';
 import { PATH_DASHBOARD } from "./routes/paths";
 
 // export const BASE_URL = "http://localhost:3001"
-export const BASE_URL = "https://chatter-box-server.vercel.app"
+export const BASE_URL = "https://chatterbox-v1.onrender.com/"
 
 
 export const defaultSettings = {
